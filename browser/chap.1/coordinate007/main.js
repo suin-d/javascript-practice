10257c78b5beeaa960fddc3a3ad22b4f34b21234
suin-d <everytheone@gmail.com>
2021-02-21 17:40:24 +0900

    DOM

--- browser/chap.1/coordinate007/main.js
+++ browser/chap.1/coordinate007/main.js
@@ -3,19 +3,33 @@ const horizontal = document.querySelector(".horizontal");
 const target = document.querySelector(".target");
 const tag = document.querySelector(".tag");
 
-document.addEventListener("mousemove", (e) => {
-  const x = e.clientX;
-  const y = e.clientY;
-  console.log(`${x}, ${y}`);
+// performance 개발 툴에서 성능 확인 가능
+addEventListener("load", () => {
+  // window가 전부 다 로드가 되면 그때 target의 크기를 읽어와서 document에 mousemove라는 event 등록
+  const targetRect = target.getBoundingClientRect();
+  const targetHalfWidth = targetRect.width / 2;
+  const targetHalfHeight = targetRect.height / 2;
 
-  vertical.style.left = `${x}px`;
-  horizontal.style.top = `${y}px`;
+  document.addEventListener("mousemove", (e) => {
+    const x = e.clientX;
+    const y = e.clientY;
+    console.log(`${x}, ${y}`);
 
-  target.style.left = `${x}px`;
-  target.style.top = `${y}px`;
+    vertical.style.transform = `translateX(${x}px)`;
+    horizontal.style.transform = `translateY(${y}px)`;
+    // vertical.style.left = `${x}px`;
+    // horizontal.style.top = `${y}px`;
 
-  tag.style.left = `${x}px`;
-  tag.style.top = `${y}px`;
+    target.style.transform = `translate(${x - targetHalfWidth}px, ${
+      y - targetHalfHeight
+    }px)`;
+    // target.style.left = `${x}px`;
+    // target.style.top = `${y}px`;
 
-  tag.textContent = `${x}, ${y}`;
+    tag.style.transform = `translate(${x}px, ${y}px)`;
+    // tag.style.left = `${x}px`;
+    // tag.style.top = `${y}px`;
+
+    tag.textContent = `${x}px, ${y}px`;
+  });
 });
